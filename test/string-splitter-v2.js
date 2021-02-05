a7d046e845119d5966ea8428018947f5151e57e1
Daniel Frost <daniel.frost@sky.uk>
2021-02-05 17:41:43 +0000

    corret solution with added comments

--- test/string-splitter-v2.js
+++ test/string-splitter-v2.js
@@ -1,3 +1,65 @@
 var assert = require("chai").assert;
 var expect = require("chai").expect;
 
+
+class StringSplitter//Sel's solution
+{
+    splitString( stringToSplit )
+    {
+        var result = [];
+
+        if( stringToSplit.length > 0 )
+            result.push(stringToSplit);
+
+        return result;
+    }
+
+}
+/*class StringSplitter //My solution
+{
+    splitString( stringToSplit )
+    {
+        
+        var result = [];
+         result.push(stringToSplit);
+
+        if( stringToSplit.length < 1)
+            result = [];
+
+        return result;
+    }
+    
+}*/
+
+describe('Splitting a string', function() {
+    it('Input an empty string, return an empty array []', function() {
+        // arrange...
+        var cut = new StringSplitter();
+        var expectedResult = []
+        var emptyString = "";
+
+        // act...
+        var actualResult = cut.splitString( emptyString );
+        console.log( actualResult.length);
+
+        // assert...
+        assert.equal(actualResult.length, expectedResult.length );
+        expect(actualResult).to.eql(expectedResult);
+    });
+
+    it('Input one string, return array with one item', function() {
+        // arrange...
+        var cut = new StringSplitter();
+        var expectedResult = ["java"]
+        var emptyString = "java";
+
+        // act...
+        var actualResult = cut.splitString( emptyString );
+        console.log( actualResult.length);
+
+        // assert...
+        assert.equal(actualResult.length, expectedResult.length );
+        expect(actualResult).to.eql(expectedResult);
+    });
+
+});
